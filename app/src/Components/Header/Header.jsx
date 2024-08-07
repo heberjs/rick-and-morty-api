99df4fc9b4040509bc797e1a8d22799ad8810b5b
heberjs <heberjsanchez@gmail.com>
2024-08-07 17:05:43 +0200

    changes url images

--- app/src/Components/Header/Header.jsx
+++ app/src/Components/Header/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
                     className='my-2 lg:hidden focus:outline-none'
                     aria-label="Toggle menu"
                 >
-                    <img className='w-8 h-8' src={toggleLink ? "/public/x.png" : "/public/menu-hamb.png"} alt={toggleLink ? "close menu" : "open menu"} />
+                    <img className='w-8 h-8' src={toggleLink ? "/x.png" : "/menu-hamb.png"} alt={toggleLink ? "close menu" : "open menu"} />
                 </button>
             </div>
 
@@ -30,11 +30,11 @@ const Header = () => {
                 <nav className="flex flex-col lg:flex-row lg:gap-4">
                     <Link to={paths.episodeList} className="flex items-center gap-1 py-2 lg:py-0 ">
                         <h2 className="font-bold md:text-2xl lg:text-3xl text-white">Episodes</h2>
-                        <img className="w-5 h-5 mt-0.5" src="/public/tv.png" alt="tv-episode" />
+                        <img className="w-5 h-5 mt-0.5" src="/tv.png" alt="tv-episode" />
                     </Link>
                     <Link to={paths.locationList} className="flex items-center gap-1 py-2 lg:py-0 ">
                         <h2 className="font-bold md:text-2xl lg:text-3xl text-white ">Location</h2>
-                        <img className="w-5 h-5 mt-0.5" src="/public/pin.png" alt="pin location" />
+                        <img className="w-5 h-5 mt-0.5" src="/pin.png" alt="pin location" />
                     </Link>
                 </nav>
             </div>
